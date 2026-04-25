43b82d0a54e0c73f0866b897a72fe28c4ab4e4a7
kibendar <danyakononenko14@gmail.com>
2026-04-25 21:47:08 +0300

    edit: turn off status bar at the start of tmux launch

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,14 +3,6 @@
 -- Add any additional options here
 --
 
--- Defer clipboard init so wl-paste isn't probed on startup while empty
-vim.opt.clipboard = ""
-vim.api.nvim_create_autocmd("User", {
-  pattern  = "VeryLazy",
-  once     = true,
-  callback = function() vim.opt.clipboard = "unnamedplus" end,
-})
-
 vim.opt.guicursor = table.concat({
   "n-v-c:block", -- Normal/Visual/Command: solid block
   "i-ci-ve:blinkon1-blinkoff1-blinkwait1-block-Cursor", -- Insert: blinking block
