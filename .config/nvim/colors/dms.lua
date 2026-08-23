d0eeed28d43f8e70df2e3a8db1700bbc01db72ad
kibendar <danyakononenko14@gmail.com>
2026-08-23 15:42:21 +0300

    feat: add matugen adaptation of theme for superfile

--- .config/nvim/colors/dms.lua
+++ .config/nvim/colors/dms.lua
@@ -86,9 +86,9 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#3a4150", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#ffd866", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#111318")
+		harmonized = base46.theme_set_bg(harmonized, "#19181a")
 	end
 
 	base46.theme_tables[theme_name] = harmonized
